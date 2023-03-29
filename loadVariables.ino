4359894bd4d63fe3e41887ad4a1a58b086ac972d
JukovYuri <122176401+MaxZhukau@users.noreply.github.com>
2023-03-29 20:27:59 +0300

    UpdateEEPROM

--- loadVariables.ino
+++ loadVariables.ino
@@ -5,6 +5,6 @@ EEPROM.get(210,  minSpeed);
 EEPROM.get(220, maxSpeed);
 EEPROM.get(230, TH_LINE);
 EEPROM.get(240,  NOISE);
-EEPROM.get(250,  kp );
-EEPROM.get(260, kd);
+EEPROM.get(245,  kp );
+EEPROM.get(2 46, kd);
 }
--- saveVariable.ino
+++ saveVariable.ino
@@ -5,6 +5,6 @@ EEPROM.put(210,  minSpeed);
 EEPROM.put(220, maxSpeed);
 EEPROM.put(230, TH_LINE);
 EEPROM.put(240,  NOISE);
-EEPROM.put(250,  kp );
-EEPROM.put(260, kd);
+EEPROM.put(245,  kp );
+EEPROM.put(246, kd);
 }
